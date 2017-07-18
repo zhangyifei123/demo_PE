e97bb3c8bca7e909f6c256d0f35d4156a4534f7c
majinghaoren <2811063089@qq.com>
2017-07-18 14:29:33 +0800

    Signed-off-by: majinghaoren <2811063089@qq.com>

--- js/attention.js
+++ js/attention.js
@@ -1,12 +1,20 @@
- function attention(){
-    $("#myAttention").css({"border-bottom":"2px solid red","color":"red"});
-    $("#myFans").css({"border-bottom":"none","color":"#333"});
- }
-function fans(){
-    $("#myFans").css({"border-bottom":"2px solid red","color":"red"});
-    $("#myAttention").css({"border-bottom":"none","color":"#333"});
-}
-var mySwiper = new Swiper ('.swiper-container', {
-        // direction: 'vertical',
-        // loop: true,
+//  function attention(){
+//     $("#myAttention").css({"border-bottom":"2px solid red","color":"red"});
+//     $("#myFans").css({"border-bottom":"none","color":"#333"});
+//  }
+// function fans(){
+//     $("#myFans").css({"border-bottom":"2px solid red","color":"red"});
+//     $("#myAttention").css({"border-bottom":"none","color":"#333"});
+// }
+var mySwiper = new Swiper('.swiper-container',{
+    onSlideChangeEnd: function(swiper){
+
+      $('.segment').removeClass('seg_highlight');
+      $($('.segment')[swiper.activeIndex]).addClass('seg_highlight')
+    }
 })
+function gotoIndex(ele,index){
+    $('.segment').removeClass('seg_highlight');
+    $(ele).addClass('seg_highlight');
+    mySwiper.slideTo(index, 500, false);//切换到第一个slide，速度为1秒
+}
